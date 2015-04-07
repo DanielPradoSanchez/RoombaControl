8e6b445bb8c9a2c31d8b1cb8c783f8e81e1f8890
DanielPradoSanchez <pradosan@mit.edu>
2015-04-07 16:15:06 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,67 +1,71 @@
 def parseRoombaSensors(sensorList):
-
-    #Bumps and Wheeldrops 0-31
-    bumps_wheeldrops = 0
-    bump_right = 0
-    bump_left = 0
-    wheeldrop_right = 0
-    wheeldrop_left = 0
-    wheeldrop_caster = 0
-
-    wall = 0
-    cliff_left = 0
-    cliff_front_left = 0
-    cliff_front_right = 0
-    cliff_right = 0
-    virtual_wall = 0
-
-    #Motor Overcurrents 0-31
-    motor_overcurrents = 0
-    side_brush = 0
-    vacuum = 0
-    main_brush = 0
-    drive_right = 0
-    drive_left = 0
-
-
-    dirt_detector_left = 0 #255
-    dirt_detector_right = 0 #255
-    remote_control_command = 0 #255
-
-
-    #Buttons 0-15
-    buttons = 0
-    max_button = 0
-    clean = 0
-    spot = 0
-    power = 0
-
-    distance1 = 0 #-32768-32767 signed 2 bytes
-    distance2 = 0
-    angle1 = 0 #-32768-32767 signed 2 bytes
-    angle2 = 0
-    charging_state = 0 #0-5
-    voltage1 = 0 #0-65535 2 bytes
-    voltage2 = 0
-    current1 = 0 #-32768-32767 signed 2 bytes
-    current2 = 0
-    temperature = 0 #-128-127 signed
-    charge1 = 0 #0-65535 2 bytes
-    charge2 = 0
-    capacity1 = 0 #0-65535 2 bytes
-    capacity2 = 0
-    sensors = [bumps_wheeldrops, wall, cliff_left,
-               cliff_front_left, cliff_front_right,
-               cliff_right, virtual_wall, motor_overcurrents,
-               dirt_detector_left, dirt_detector_right,
-               remote_control_command, buttons, distance1, distance2,
-               angle1, angle2, charging_state, voltage1, voltage2,
-               current1, current2, temperature, charge1, charge2,
-               capacity1, capacity2]
-    for i in range(len(sensorList)):
-        sensors[i] = ord(sensorList[i])
+##    Sensors
+##
+##    
+##    #Bumps and Wheeldrops 0-31
+##    bumps_wheeldrops
+##    bump_right
+##    bump_left
+##    wheeldrop_right
+##    wheeldrop_left
+##    wheeldrop_caster
+##
+##    wall
+##    cliff_left
+##    cliff_front_left
+##    cliff_front_right
+##    cliff_right
+##    virtual_wall
+##
+##    #Motor Overcurrents 0-31
+##    motor_overcurrents
+##    side_brush
+##    vacuum
+##    main_brush
+##    drive_right
+##    drive_left
+##
+##
+##    dirt_detector_left #255
+##    dirt_detector_right #255
+##    remote_control_command #255
+##
+##
+##    #Buttons 0-15
+##    buttons
+##    max_button
+##    clean
+##    spot
+##    power
+##
+##    distance1 #-32768-32767 signed 2 bytes
+##    distance2
+##    angle1 #-32768-32767 signed 2 bytes
+##    angle2
+##    charging_state #0-5
+##    voltage1 #0-65535 2 bytes
+##    voltage2
+##    current1 #-32768-32767 signed 2 bytes
+##    current2
+##    temperature #-128-127 signed
+##    charge1 #0-65535 2 bytes
+##    charge2
+##    capacity1 #0-65535 2 bytes
+##    capacity2
     
-    bumps_wheeldrops = '{0:05b}'.format(sensors[0])
+    sensors = ["bumps_wheeldrops", "wall", "cliff_left",
+               "cliff_front_left", "cliff_front_right",
+               "cliff_right", "virtual_wall", "motor_overcurrents",
+               "dirt_detector_left", "dirt_detector_right",
+               "remote_control_command", "buttons", "distance1", "distance2",
+               "angle1", "angle2", "charging_state", "voltage1", "voltage2",
+               "current1", "current2", "temperature", "charge1", "charge2",
+               "capacity1", "capacity2"]
+    sensorMapping = {}
+    for i in range(26):
+        sensorMapping[sensors[i]] = ord(sensorList[i])
+    
+    bumps_wheeldrops = '{0:05b}'.format(sensorMapping[sensors[0]])
     bump_right = int(bumps_wheeldrops[4])
     bump_left = int(bumps_wheeldrops[3])
     wheeldrop_right = int(bumps_wheeldrops[2])
@@ -69,7 +73,7 @@ def parseRoombaSensors(sensorList):
     wheeldrop_caster = int(bumps_wheeldrops[0])
 
     
-    motor_overcurrents = '{0:05b}'.format(sensors[7])
+    motor_overcurrents = '{0:05b}'.format(sensorMapping[sensors[7]])
     side_brush = int(motor_overcurrents[4])
     vacuum = int(motor_overcurrents[3])
     main_brush = int(motor_overcurrents[2])
@@ -77,23 +81,43 @@ def parseRoombaSensors(sensorList):
     drive_left = int(motor_overcurrents[0])
 
     
-    buttons = '{0:04b}'.format(sensors[0])
+    buttons = '{0:04b}'.format(sensorMapping[sensors[0]])
     max_button = int(buttons[3])
     clean = int(buttons[2])
     spot = int(buttons[1])
     power = int(buttons[0])
 
-    parsed_sensors = [bump_right, bump_left, wheeldrop_right,
-                      wheeldrop_left, wheeldrop_caster, wall, cliff_left,
-                      cliff_front_left, cliff_front_right, cliff_right,
-                      virtual_wall, side_brush, vacuum, main_brush,
-                      drive_right, drive_left, dirt_detector_left,
-                      dirt_detector_right, remote_control_command, max_button,
-                      clean, spot, power, distance1, distance2,
-                      angle1, angle2, charging_state, voltage1, voltage2,
-                      current1, current2, temperature, charge1, charge2,
-                      capacity1, capacity2]
-    return sensors
+    parsedSensorList = ["bump_right", "bump_left", "wheeldrop_right",
+                      "wheeldrop_left", "wheeldrop_caster", "wall, cliff_left",
+                      "cliff_front_left", "cliff_front_right", "cliff_right",
+                      "virtual_wall", "side_brush", "vacuum", "main_brush",
+                      "drive_right", "drive_left", "dirt_detector_left",
+                      "dirt_detector_right", "remote_control_command", "max_button",
+                      "clean", "spot", "power", "distance1", "distance2",
+                      "angle1", "angle2", "charging_state", "voltage1", "voltage2",
+                      "current1", "current2", "temperature", "charge1", "charge2",
+                      "capacity1", "capacity2"]
+    
+    parsedSensors = [bump_right, bump_left, wheeldrop_right,
+                      wheeldrop_left, wheeldrop_caster, sensorMapping["wall"],
+                      sensorMapping["cliff_left"], sensorMapping["cliff_front_left"],
+                      sensorMapping["cliff_front_right"], sensorMapping["cliff_right"],
+                      sensorMapping["virtual_wall"], side_brush, vacuum, main_brush,
+                      drive_right, drive_left, sensorMapping["dirt_detector_left"],
+                      sensorMapping["dirt_detector_right"],
+                      sensorMapping["remote_control_command"], max_button,
+                      clean, spot, power, sensorMapping["distance1"],
+                      sensorMapping["distance2"], sensorMapping["angle1"],
+                      sensorMapping["angle2"], sensorMapping["charging_state"],
+                      sensorMapping["voltage1"], sensorMapping["voltage2"],
+                      sensorMapping["current1"], sensorMapping["current2"],
+                      sensorMapping["temperature"], sensorMapping["charge1"],
+                      sensorMapping["charge2"], sensorMapping["capacity1"],
+                      sensorMapping["capacity2"]]
+    parsedSensorMapping = {}
+    for i in range(len(parsedSensorList)):
+        parsedSensorMapping[parsedSensorList[i]] = parsedSensors[i] 
+    return parsedSensorMapping
 
 
 
